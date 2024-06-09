8ca158663e88bd1ecc399abfe08d22ff4e4b02bf
STSchiff <6112968+chrisheib@users.noreply.github.com>
2024-06-09 03:59:34 +0200

    basic onsceneloaded

--- questpost/Plugin.cs
+++ questpost/Plugin.cs
@@ -1,14 +1,53 @@
 ﻿using BepInEx;
+using Wish;
+using HarmonyLib;
+using UnityEngine.SceneManagement;
 
 namespace questpost
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+
+        // private Harmony m_harmony = new Harmony("stschiff.questpost");
+
         private void Awake()
         {
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            // ScenePortalManager.onLoadedScene += OnSceneLoaded;
+            SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+            // this.m_harmony.PatchAll();
         }
+
+        public void AddBoard()
+        {
+            Logger.LogInfo($"Scene {ScenePortalManager.ActiveSceneName} ({ScenePortalManager.ActiveSceneIndex}) loaded!");
+            // Instantiate(Wish.BulletinBoard, ScenePortalManager.)
+            // TODO: Instantiate with scene parent :) 
+            // Set location based on activescenename
+        }
+        public void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+        {
+            Logger.LogInfo($"Scene {arg0.name}, {arg0.path} |old: {ScenePortalManager.ActiveSceneName} ({ScenePortalManager.ActiveSceneIndex}) loaded, mode {arg1}!");
+            if (arg0.name == "Tier1House0")
+            {
+                Logger.LogInfo("house load detected!");
+                ScenePortalManager.onLoadedScene += AddBoard;
+            }
+        }
+
+        //[HarmonyPatch(typeof(load), "Awake")]
+        //class HarmonyPatch_Pickup_Awake
+        //{
+
+        //    private static void Postfix(ref float ___radius)
+        //    {
+        //        if (m_enabled.Value)
+        //        {
+        //            ___radius = m_pickup_radius.Value;
+        //        }
+        //    }
+        //}
     }
 }
--- questpost/readme.md
+++ questpost/readme.md
@@ -1,1 +1,18 @@
 requires SunHaven.Core.dll in directory
+
+https://docs.bepinex.dev/articles/user_guide/installation/index.html
+
+https://github.com/BepInEx/HarmonyX/wiki/Basic-usage
+
+https://github.com/devopsdinosaur/sunhaven-mods
+
+https://github.com/expired6978/sunhaven
+
+https://github.com/Morthy/sunhaven-mods
+
+https://github.com/Morthy/sunhaven-mods/blob/15a5555e31610e7213b2ff697ab757d6dd7daf19/MuseumTracker/Plugin.cs#L614
+https://github.com/Morthy/sunhaven-mods/blob/15a5555e31610e7213b2ff697ab757d6dd7daf19/MuseumTracker/Plugin.cs#L588
+
+v2019.3.15.5898046
+
+dotnet new bepinex5plugin -n questpost -T netstandard2.0 -U v2019.3.15.5898046
